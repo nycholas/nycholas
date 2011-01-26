9b63e7a9224b9a00139e0bca84da408ea2098c85
Nycholas de Oliveira e Oliveira <nycholas@gmail.com>
2011-01-26 16:52:49 -0200

    test irc

--- projects/ask-undrgz/bin/teste-irc.py
+++ projects/ask-undrgz/bin/teste-irc.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # ask-undrgz system of questions uses data from underguiz.
-# Copyright (c) 2010, Nycholas de Oliveira e Oliveira <nycholas@gmail.com>
+# Copyright (c) 2011, Nycholas de Oliveira e Oliveira <nycholas@gmail.com>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -17,7 +17,7 @@
 #    its contributors may be used to endorse or promote products derived from
 #    this software without specific prior written permission.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
 # ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
@@ -31,22 +31,35 @@
 import sys
 import socket
 import string
+import random
 
-NICK="undrz-answers"
-IDENT="undrz-answers"
-REALNAME="undrz-answers"
-readbuffer=""
-CHANNELINIT="#ask-undrz"
+NICK='ask-undrgz'
+IDENT='ask-undrgz'
+REALNAME='ask-undrgz'
+CHANNEL='#moonstonemedical'
 
-server = socket.socket()
-server.connect(("irc.freenode.net", 6667))
-server.send("NICK undrz-answers\r\n")
-server.send("USER undrz-answers undrz-answers :undrz-answers\r\n")
-server.send('JOIN #ask-undrz\r\n')
-server.send('NOTICE underguiz WTF?\r\n')
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect(('irc.freenode.net', 6667))
+server.send('NICK %s\r\n' % (NICK,))
+server.send('USER %s %s ask-undrgz.appspot.com %s\r\n' % (NICK, IDENT, REALNAME))
+server.send('JOIN %s\r\n' % (CHANNEL,))
 
 while True:
     data = server.recv(1024)
+    data_split = data.split(':')
+
+    # Ping message
+    # data: PING :jordan.freenode.net
     if data.find('PING') != -1:
-        server.send("PONG %s\r\n" % data.split()[1])
-    print data
+        server.send('PONG %s\r\n' % data.split()[1])
+    # Private message
+    # <o_lalertom> ask-undrgz, hai!
+    # data: :o_lalertom!~Nycholas@666-666-666-666.cenobites.org PRIVMSG #moonstonemedical :ask-undrgz, hai!
+    # <o_lalertom> ha! ask-undrgz ha!
+    # data: :o_lalertom!~Nycholas@666-666-666-666.cenobites.org PRIVMSG #moonstonemedical :ha! ask-undrgz ha!
+    elif data_split[-1].find(NICK) != -1:
+        server.send('PRIVMSG %s :%s\r\n' % (CHANNEL, 'hai!'))
+    # Public message
+    # <o_lalertom> hai!
+    # data: :o_lalertom!~Nycholas@666-666-666-666.cenobites.org PRIVMSG #moonstonemedical :hai!
+    sys.stdout.write(data)
