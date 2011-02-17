a1790fe9b1905f051acca42ad219dfc5128ef056
Nycholas de Oliveira e Oliveira <nycholas@gmail.com>
2011-02-17 15:08:05 -0200

    Example socket tcp parallel in Erlang

---
+++ code/erlang/socket/examples/tcp-parallel-1/server.erl
@@ -0,0 +1,54 @@
+% Example socket tcp parallel.
+% Copyright (c) 2011, Nycholas de Oliveira e Oliveira <nycholas@gmail.com>
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+%
+% % Redistributions of source code must retain the above copyright notice,
+%    this list of conditions and the following disclaimer.
+% % Redistributions in binary form must reproduce the above copyright notice,
+%    this list of conditions and the following disclaimer in the documentation
+%    and/or other materials provided with the distribution.
+% % Neither the name of the Nycholas de Oliveira e Oliveira nor the names of
+%    its contributors may be used to endorse or promote products derived from
+%    this software without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+-module(server).
+-export([start/0]).
+
+start() ->
+    {ok, Listen} = gen_tcp:listen(8086, [binary, {packet, 4},
+                                         {reuseaddr, true},
+                                         {active, true}]),
+    spawn(fun() -> accept(Listen) end).
+
+accept(Listen) ->
+    {ok, Socket} = gen_tcp:accept(Listen),
+    spawn(fun() -> accept(Listen) end),
+    loop(Socket).
+
+loop(Socket) ->
+    receive
+        {tcp, Socket, Bin} ->
+            Str = binary_to_term(Bin),
+            Reply = Str,
+            io:format("Server received binary = ~p~n", [Bin]),
+            io:format("Server (unpacked) ~p~n", [Str]),
+            io:format("Server replying = ~p~n", [Reply]),
+            gen_tcp:send(Socket, term_to_binary(Reply)),
+            loop(Socket);
+        {tcp_closed, Socket} ->
+            io:format("Server socket closed~n")
+    end.
