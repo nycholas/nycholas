1a3b4c4d196e0737b5e38a4451c3397f9e7ec8e4
Nycholas de Oliveira e Oliveira <nycholas@gmail.com>
2011-11-15 19:58:53 -0200

    Example erlang concurrent programming with errors

---
+++ code/erlang/concurrent_programming/errors/mycalc-0/server.erl
@@ -0,0 +1,71 @@
+%%%----------------------------------------------------------------------------
+%%% Simple calculator in concurrent programming with error handling.
+%%% Copyright (c) 2011, Nycholas de Oliveira e Oliveira <nycholas@gmail.com>
+%%% All rights reserved.
+%%%
+%%% Redistribution and use in source and binary forms, with or without
+%%% modification, are permitted provided that the following conditions are met:
+%%%
+%%% % Redistributions of source code must retain the above copyright notice,
+%%%    this list of conditions and the following disclaimer.
+%%% % Redistributions in binary form must reproduce the above copyright notice,
+%%%    this list of conditions and the following disclaimer in the documentation
+%%%    and/or other materials provided with the distribution.
+%%% % Neither the name of the Nycholas de Oliveira e Oliveira nor the names of
+%%%    its contributors may be used to endorse or promote products derived from
+%%%    this software without specific prior written permission.
+%%%
+%%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+%%% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+%%% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+%%% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+%%% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+%%% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+%%% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+%%% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+%%% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+%%% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+%%% POSSIBILITY OF SUCH DAMAGE.
+%%%----------------------------------------------------------------------------
+-module(server).
+
+-revision('Revision: 0.1 ').
+-created('Date: 2011/11/13 11:04:00 ').
+-created_by('nycholas@gmail.com').
+-modified('Date: 2011/11/13 11:04:00 ').
+-modified_by('nycholas@gmail.com').
+
+-export([loop/0, on_exit/2]).
+    
+loop() ->
+    receive
+        {sum, A, B} ->
+            N = A + B,
+            io:format("  -> ~p + ~p = ~p~n", [A, B, N]),
+            loop();
+        {subtract, A, B} ->
+            N = A - B,
+            io:format("  -> ~p - ~p = ~p~n", [A, B, N]),
+            loop();
+        {multiplies, A, B} ->
+            N = A * B,
+            io:format("  -> ~p * ~p = ~p~n", [A, B, N]),
+            loop();
+        {divides, A, B} ->
+            N = A / B,
+            io:format("  -> ~p / ~p = ~p~n", [A, B, N]),
+            loop();
+        Other ->
+            io:format("  -> Not found operation: ~p~n", [Other]),
+            loop()
+    end.
+
+on_exit(Pid, Fun) ->
+    spawn(fun() ->
+        process_flag(trap_exit, true),
+        link(Pid),
+        receive
+            {'EXIT', Pid, Why} ->
+                Fun(Why)
+        end 
+    end).
