a91d72e29374d0d672effd7ca0cc0caa05ecc04c
Nycholas de Oliveira e Oliveira <nycholas@gmail.com>
2020-05-15 13:10:12 -0300

    Where do I begin?

--- courses/futurelearn/functional-programming-erlang/w2s20/example.erl
+++ courses/futurelearn/functional-programming-erlang/w2s20/example.erl
@@ -2,11 +2,15 @@
 
 -export([take/2, tests/0]).
 
+-spec take(integer(), [T]) -> [T].
 take(0, _) ->
+    [];
+take(_, []) ->
     [];
 take(N, L) when N > 0 ->
     take(N, L, []).
 
+-spec take(integer(), [T], [T]) -> [T].
 take(0, _, Acc) ->
     lists_reverse(Acc);
 take(_, [], Acc) ->
@@ -14,16 +18,20 @@ take(_, [], Acc) ->
 take(N, [E | L], Acc) ->
     take(N - 1, L, [E | Acc]).
 
+-spec tests() -> ok.
 tests() ->
     [] = take(0, "hello"),
+    [] = take(10, []),
     "hell" = take(4, "hello"),
     "hello" = take(5, "hello"),
     "hello" = take(9, "hello"),
     ok.
 
+-spec lists_reverse([T]) -> [T].
 lists_reverse(L) ->
     lists_reverse(L, []).
 
+-spec lists_reverse([T], [T]) -> [T].
 lists_reverse([], Acc) ->
     Acc;
 lists_reverse([E | L], Acc) ->
